8cebf73e85d2cc6943c99fe0f6349367b43d0195
lxnny <89148963+lxnnydev@users.noreply.github.com>
2023-03-02 19:39:14 +0100

    Update keysys_backup.lua

--- keysys_backup.lua
+++ keysys_backup.lua
@@ -35,6 +35,7 @@ local UIAspectRatioConstraint_10 = Instance.new("UIAspectRatioConstraint")
 
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 ScreenGui.ResetOnSpawn = false
+ScreenGui.DisplayOder = 100
 
 Key.Name = "Key"
 Key.Parent = ScreenGui
