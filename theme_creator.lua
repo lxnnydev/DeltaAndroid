e67a5f7f613b483169fab6aa6ade4d1b79b643e0
lxnny <89148963+lxnnydev@users.noreply.github.com>
2023-04-01 21:16:59 +0200

    Update theme_creator.lua

--- theme_creator.lua
+++ theme_creator.lua
@@ -3,6 +3,31 @@ local Window = Library.CreateLib("Delta Custom Theme Creator", "Ocean")
 local Tab = Window:NewTab("Main")
 local Section = Tab:NewSection("Create a Theme")
 
+function toHex(color)
+	local int = toInteger(color)
+	
+	local current = int
+	local final = ""
+	
+	local hexChar = {
+		"A", "B", "C", "D", "E", "F"
+	}
+	
+	repeat local remainder = current % 16
+		local char = tostring(remainder)
+		
+		if remainder >= 10 then
+			char = hexChar[1 + remainder - 10]
+		end
+		
+		current = math.floor(current/16)
+		final = final..char
+	until current <= 0
+	
+	return "#"..string.reverse(final)
+end
+
+
 local BgColor
 local BtnColor
 local MenuColor
@@ -25,18 +50,18 @@ game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Playe
 end)
 
 Section:NewColorPicker("Background Color", "Color Info", Color3.fromHex("#141417"), function(color)
-    BgColor = color
+    BgColor = toHex(color)
 end)
 
 Section:NewColorPicker("Button Color", "Color Info", Color3.fromHex("#8F82FF"), function(color)
-    BtnColor = color
+    BtnColor = toHex(color)
 end)
 
 Section:NewColorPicker("Menu Color", "Color Info", Color3.fromHex("#070708"), function(color)
-    MenuColor = color
+    MenuColor = toHex(color)
 end)
 
 Section:NewColorPicker("Settings Input Color", "Color Info", Color3.fromHex("#1E1E1E"), function(color)
-    SettingBtnColor = color
+    SettingBtnColor = toHex(color)
 end)
 
