5193c37a64a99f8017cefbe85a0fa137805685d1
lxnny <89148963+lxnnydev@users.noreply.github.com>
2023-06-12 15:18:59 +0200

    Update new_main_ui.lua

--- new_main_ui.lua
+++ new_main_ui.lua
@@ -23,9 +23,9 @@ getgenv().version_x_beta = function()
 	if(not isfile("is_versx_beta")) then
 		writefile("is_versx_beta", "true")
 		wait()
-		game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
 	end
     loadstring(game:HttpGet("https://gist.githubusercontent.com/lxnnydev/24afeb5cda142560e2e6cf3a727f6c5c/raw/7d99ea12eeaaa822b5a8ed80048dda85a09dc789/version_x_beta.lua",true))()
+    return
 end
 
 if(isfile("is_versx_beta")) then
